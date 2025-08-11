087e0e919c3347a1cca148c45f38856d61d2d9f6
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-08-12 05:02:57 +0600

    all bug fix

--- wp-content/themes/kadence-child/templates/single-page-checkout/main.php
+++ wp-content/themes/kadence-child/templates/single-page-checkout/main.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 <div id="spc-toast-container" class="spc-toast-container"></div>
 
 <div id="single-page-checkout-container" class="spc-container">
-    <h1 class="spc-section-title">
+    <h1 class="spc-section-title title_h1">
         <svg width="36" height="36" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji"
             preserveAspectRatio="xMidYMid meet" fill="#000000">
